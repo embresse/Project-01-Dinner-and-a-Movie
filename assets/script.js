be330892400f69467f01cfa57ff3b5745d6c1c1c
Orlando Nelson <nelson8223@gmail.com>
2022-12-19 16:55:42 -0500

    added syntext to JS

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,35 @@
 var button= document.querySelector('.button')
-var inputValue = document.querySelector('inputValue')
-var desc = document.querySelector('desc');
-var temp = document.querySelector('.temp')
+var inputValue = document.querySelector('#inputValue')
+var resultContainer= document.querySelector('#weather-container')
+var city = document.createElement('p')
+resultsContainer.append(city)
+
+button.addEventListener('click',function(event){
+    event.preventDefault
+    console.log(inputValue.value)
+    
 
 
-
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Richmond&units=imperial&appid=65ea356fac868b7989b751cef0bfce08')
+     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + '&appid=65ea356fac868b7989b751cef0bfce08')
     .then((response) => response.json())
     .then((data) => console.log(data));
 
-    button.addEventListener(click)
 
- 
+
+then((response) => response.json())
+.then((data) => { 
+  console.log(data)
+  console.log(data.main.temp)
+
+   
+})
+
+    var city= document.createElement('p');
+   city.textContent= weather.data;
+   resultsContainer.append(city);
+
+   var temp  = document.createElement("p");
+   temp.textContent = "temp:" + data.main.temp+ "°F";
+   resultsContainer.append(temp);
+
+})
